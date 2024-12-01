45946fc29686bfc2a3ffbc642ff900fd8e6be5b5
nils-matthaei <n.matthaei@proton.me>
2024-12-01 19:02:18 +0100

    Finished day1

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Advent of Code 2024
 
-- [ ] day 1
+- [x] day 1
 - [ ] day 2
 - [ ] day 3
 - [ ] day 4
--- src/day1.rs
+++ src/day1.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use crate::common::read_lines;
 
 const SAMPLE_PATH: &str = "./input/day1/riddle1.txt";
@@ -5,6 +7,7 @@ const SAMPLE_PATH: &str = "./input/day1/riddle1.txt";
 pub fn solve() {
     let mut left_ids: Vec<i32> = vec![];
     let mut right_ids: Vec<i32> = vec![];
+    let mut count_ids: HashMap<i32, i32> = HashMap::new();
 
     if let Ok(lines) = read_lines(SAMPLE_PATH) {
         for line in lines.flatten() {
@@ -14,16 +17,23 @@ pub fn solve() {
                 .collect();
             left_ids.push(numbers[0]);
             right_ids.push(numbers[1]);
+            let counter = count_ids.entry(numbers[1]).or_insert(0);
+            *counter += 1;
         }
 
         left_ids.sort();
         right_ids.sort();
 
         let mut dist: i32 = 0;
+        let mut similarity_score: i32 = 0;
         for i in 0..left_ids.len() {
             dist += (left_ids[i] - right_ids[i]).abs();
+            similarity_score += left_ids[i] * *count_ids.entry(left_ids[i]).or_insert(0);
         }
 
-        println!("{}", dist);
+        println!(
+            "Distance: {}\n Similarity Score: {}",
+            dist, similarity_score
+        );
     }
 }
