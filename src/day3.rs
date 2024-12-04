1e685f751562b1d2c3ec0462add334b99a34fa75
nils-matthaei <n.matthaei@proton.me>
2024-12-04 09:57:32 +0100

    Finished day3

--- src/day3.rs
+++ src/day3.rs
@@ -2,6 +2,7 @@ use crate::common::read_lines;
 use regex::Regex;
 
 const SAMPLE_PATH: &str = "./input/day3/sample.txt";
+const SAMPLE_PATH2: &str = "./input/day3/sample2.txt";
 const INPUT_PATH: &str = "./input/day3/input.txt";
 
 const PATTERN: &str = r"mul\((\d+),(\d+)\)";
@@ -17,12 +18,38 @@ fn execute_multiplication(line: &str) -> i32 {
     return sum;
 }
 
-pub fn solve() {
-    let mut result = 0;
-    if let Ok(lines) = read_lines(INPUT_PATH) {
-        for line in lines.flatten() {
-            result += execute_multiplication(&line);
+const PATTERN_DO: &str = r"do\(\)|don't\(\)|mul\((\d+),(\d+)\)";
+
+fn there_is_no_try(line: &str, global_active: bool) -> (i32, bool) {
+    let mut sum = 0;
+    let mut active = global_active;
+    let re = Regex::new(PATTERN_DO).unwrap();
+    for caps in re.captures_iter(line) {
+        match &caps[0] {
+            "do()" => active = true,
+            "don't()" => active = false,
+            _ => {
+                if active {
+                    let first: i32 = caps[1].parse::<i32>().unwrap();
+                    let second: i32 = caps[2].parse::<i32>().unwrap();
+                    sum += first * second;
+                }
+            }
         }
     }
-    println!("Multiplication Result: {}", result);
+    return (sum, active);
+}
+
+pub fn solve() {
+    let mut sum = 0;
+    let mut global_active = true;
+    if let Ok(lines) = read_lines(INPUT_PATH) {
+        for line in lines.flatten() {
+            //result += execute_multiplication(&line);
+            let result = there_is_no_try(&line, global_active);
+            sum += result.0;
+            global_active = result.1;
+        }
+    }
+    println!("Multiplication Result: {}", sum);
 }
